d2bbdc0c9869035b19c26522d032652d327dac26
doritmtm <dorian_tm@yahoo.com>
2024-05-03 00:49:35 +0300

    Update Stop_MsDeploy.ps1

--- DarimarSystemWebsite/Scripts/Stop_MsDeploy.ps1
+++ DarimarSystemWebsite/Scripts/Stop_MsDeploy.ps1
@@ -1,10 +1,13 @@
 $msdeploy = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe";
 
 $offlinePath = $args[0]
-$hostSiteName = $args[1]
-$computerName = $args[2]
-$username = $args[3]
-$password = $args[4]
+$publishBinPath = $args[1]
+$hostSiteName = $args[2]
+$computerName = $args[3]
+$username = $args[4]
+$password = $args[5]
+
+Copy-Item "${offlinePath}\app_offline.htm" -Destination $publishBinPath
 
 $computerNameArgument = $computerName + '/MsDeploy.axd?site=' + $hostSiteName
 
