3b77de8dc1f6c9870bd203676ae57938a86c3c9e
doritmtm <dorian_tm@yahoo.com>
2024-05-03 00:41:10 +0300

    Update Start_MsDeploy.ps1

--- DarimarSystemWebsite/Scripts/Start_MsDeploy.ps1
+++ DarimarSystemWebsite/Scripts/Start_MsDeploy.ps1
@@ -1,9 +1,9 @@
 $msdeploy = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe";
 
-$hostSiteName = $args[1]
-$computerName = $args[2]
-$username = $args[3]
-$password = $args[4]
+$hostSiteName = $args[0]
+$computerName = $args[1]
+$username = $args[2]
+$password = $args[3]
 
 $computerNameArgument = $computerName + '/MsDeploy.axd?site=' + $hostSiteName
 
